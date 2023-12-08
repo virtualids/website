71f7dddf05017c90407bd769707ee39c3b85ad53
Brody Carty <61958601+bordolbg@users.noreply.github.com>
2023-12-08 12:50:27 -0600

    create new folder and page for submission

---
+++ src/routes/pireps/submission/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+	<title>PIREP Submission</title>
+	<meta name="PIREP Submission" content="PIREP Submissin" />
+</svelte:head>
+
+<section>
+
+
+
+</section>
+
+<style>
+    
+</style>
